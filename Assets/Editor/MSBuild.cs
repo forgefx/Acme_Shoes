bfd05f92c1dd512b6dff598c9d55f2e392134e0e
AdamKane <akane@forgefx.com>
2021-12-02 02:52:59 -1000

    [master] Successfully getting .appx output from 'Build-fromEditorVSPROJBuildScript" folder contents.

--- Assets/Editor/MSBuild.cs
+++ Assets/Editor/MSBuild.cs
@@ -15,7 +15,7 @@ public static class MSBuild
         var arguments = new List<string>();
 
         // What to build.
-        arguments.Add(@"C:\Repos\HoloLens-SpinningCube\Build-fromEditor\HoloLens-SpinningCube.sln");
+        arguments.Add(@"C:\Repos\HoloLens-SpinningCube\Build-fromEditorVSPROJBuildScript\HoloLens-SpinningCube.sln");
 
         // Validate the project file and, if validation succeeds, build the project. If you don't specify schema, the project is validated against the default schema.
         //arguments.Add("-validate"); 
