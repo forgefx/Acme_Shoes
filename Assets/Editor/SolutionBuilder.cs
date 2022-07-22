b1005d3d8dee9e39f7c46e080effb0a1ca26ab3e
AdamKane <akane@forgefx.com>
2022-07-21 23:24:56 -1000

    Added buildPlayerOptions.locationPathName = $@"../Builds/{appVersion}";

--- Assets/Editor/SolutionBuilder.cs
+++ Assets/Editor/SolutionBuilder.cs
@@ -1,37 +1,30 @@
-﻿using System;
-using System.Diagnostics;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
-using System.Collections.Generic;
-using BuildScripts;
 
 public static class SolutionBuilder
 {
     [MenuItem("BuildTests/SolutionBuilder.Build()")]
     public static void Build()
     {
-        {
-            var foo = BuildScripts.BuildAgentInfo.GetInfo();
-            var bar = BuildAgentInfo.TryGetPathToUWPEditorExtensions();
-            
-            // Build solution.
-            EditorUserBuildSettings.wsaSubtarget = WSASubtarget.HoloLens;
-            EditorUserBuildSettings.wsaUWPBuildType = WSAUWPBuildType.D3D;
+        // Application version.
+        string appVersion = Application.version;
+        
+        // Build solution.
+        EditorUserBuildSettings.wsaSubtarget = WSASubtarget.HoloLens;
+        EditorUserBuildSettings.wsaUWPBuildType = WSAUWPBuildType.D3D;
 
-            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.scenes = new[] {"Assets/Scenes/SampleScene.unity"};
-            buildPlayerOptions.locationPathName = @"../Builds"; // was: @"C:/Builds";
-            buildPlayerOptions.target = BuildTarget.WSAPlayer;
-            buildPlayerOptions.options = BuildOptions.None; // BuildOptions.AutoRunPlayer; // In theory, this causes .appx to be generated.
-            //buildPlayerOptions.targetGroup = BuildTargetGroup.WSA;
-            
-            Debug.Log("[FFX] BuildPipeline.BuildPlayer(buildPlayerOptions);");
-            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = $@"../Builds/{appVersion}"; // was: @"C:/Builds";
+        buildPlayerOptions.target = BuildTarget.WSAPlayer;
+        buildPlayerOptions.options = BuildOptions.None; // BuildOptions.AutoRunPlayer; // In theory, this causes .appx to be generated.
+        //buildPlayerOptions.targetGroup = BuildTargetGroup.WSA;
 
-            // Build .appx
-            //MSBuild.ConfigureAndRun();
-        }
+        Debug.Log("[FFX] BuildPipeline.BuildPlayer(buildPlayerOptions);");
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        // Build .appx
+        //MSBuild.ConfigureAndRun();
     }
-
 }
