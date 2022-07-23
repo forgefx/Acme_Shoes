d44b65dc6e07bdadf043bf7acf4558fe7bd53cea
AdamKane <akane@forgefx.com>
2022-07-22 17:23:19 -1000

    sln

--- Assets/Editor/SolutionBuilder.cs
+++ Assets/Editor/SolutionBuilder.cs
@@ -15,7 +15,7 @@ public static class SolutionBuilder
         Debug.Log($"Product code: {productCode}");
         
         //
-        var path = $@"../Builds/{appVersion}/{productCode}"; 
+        var path = $@"../Builds/{appVersion}/{productCode}/Sln"; 
         Debug.Log($"Path: {path}");
         
         // Build solution.
