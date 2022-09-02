eb6c4cb19b9a1616edc243df3472419aa7690844
AdamKane <akane@forgefx.com>
2022-09-01 16:46:34 -1000

    buildPlayerOptions.locationPathName = "../Build-Zebra";

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -40,7 +40,7 @@ namespace BuildScripts
 
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
             buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
-            buildPlayerOptions.locationPathName = "Build-SLN"; // was: path; 
+            buildPlayerOptions.locationPathName = "../Build-Zebra"; // was: path; 
             buildPlayerOptions.target = BuildTarget.WSAPlayer;
             buildPlayerOptions.options = BuildOptions.None; // BuildOptions.AutoRunPlayer; // In theory, this causes .appx to be generated.
             //buildPlayerOptions.targetGroup = BuildTargetGroup.WSA;
